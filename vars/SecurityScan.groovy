902372070f1d715cb2f896b54fcbca801f2731d9
Temi <106712999+iAmTopeTemi@users.noreply.github.com>
2024-06-11 05:32:16 -0600

    Delete vars directory

--- vars/SecurityScan.groovy
+++
@@ -1,6 +0,0 @@
-def call(String modelPath) {
-    echo "Starting security scan for model at ${modelPath}"
-    // Load the SecurityScan class and perform the scan
-    def scanner = new org.example.SecurityScan()
-    scanner.scan(modelPath)
-}
